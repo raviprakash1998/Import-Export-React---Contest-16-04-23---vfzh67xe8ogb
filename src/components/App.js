e4c923d83cc0a9d811d55a5151ce52a5cab2f4ca
Ravi Prakash <120682913+raviprakash1998@users.noreply.github.com>
2023-04-16 20:21:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,10 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <script src="./Heading.js" type="text/javascript" />
+      <script src="./InputQuery.js" type="text/javascript" />
+      <script src="./SubHeading.js" type="text/javascript" />
+      <script src="./SubmitButton.js" type="text/javascript" />
     </div>
   )
 }
